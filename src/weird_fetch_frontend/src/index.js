e7d9a95e22bfdd331164264e6a8531becc95aa68
Kyle Peacock <kylpeacock@gmail.com>
2023-06-08 13:11:19 -0700

    Initial commit.

---
+++ src/weird_fetch_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { weird_fetch_backend } from "../../declarations/weird_fetch_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await weird_fetch_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
